061be3ec99c0b42d1019c5968b22fc34f9b23b45
Nikolai <nikocraftex@gmail.com>
2021-01-15 00:48:54 +0200

    Started working on the Lab1

---
+++ Ex1/Classes.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+class Platforms
+{
+private:
+
+
+public:
+
+
+
+
+
+};
+
+class Games
+{
+private:
+
+
+public:
+
+
+
+
+
+};
+
+class Achievments
+{
+private:
+
+
+public:
+
+
+	
+
+
+
+};
