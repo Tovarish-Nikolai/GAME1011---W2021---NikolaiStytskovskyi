cd930be383c8205087c2fe5a456f91ec9e4b8ad8
Nikolai <nikocraftex@gmail.com>
2021-01-15 01:01:26 +0200

    Created basic Class functions
    It's 1 am help

--- Ex1/Classes.h
+++ Ex1/Classes.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,39 +8,75 @@ using namespace std;
 class Platforms
 {
 private:
-
-
+	string m_name;
+	string m_manufacturer;
 public:
+	void GetName(string namePlatform)
+	{
+		m_name = namePlatform;
+	}
+
+	void GetManufacturer(string nameManu)
+	{
+		m_manufacturer = nameManu;
+	}
+
+	void CreateGameArray(int amount)
+	{
 
 
-
-
-
+	}
 };
 
 class Games
 {
 private:
-
-
+	string m_name;
+	string m_publisher;
+	string m_developer;
 public:
+	void GetName(string nameGame)
+	{
+		m_name = nameGame;
+	}
+
+	void GetPublisher(string namePublish)
+	{
+		m_publisher = namePublish;
+	}
+
+	void GetDev(string nameDev)
+	{
+		m_developer = nameDev;
+	}
+
+	void CreateAchievmentArray(int amount)
+	{
 
 
 
-
-
+	}
 };
 
 class Achievments
 {
 private:
-
-
+	string m_title;
+	string m_description;
+	int m_score;
 public:
+	void GetTitle(string nameTitle)
+	{
+		m_title = nameTitle;
+	}
 
+	void GetDescription(string descr)
+	{
+		m_description = descr;
+	}
 
-	
-
-
-
+	void GetScore(int amount)
+	{
+		m_score = amount;
+	}
 };
